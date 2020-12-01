8dc43a93fc40d130b90510d5d8a9849e16819476
Ben Fidlers <ben.fidlers@student.kuleuven.be>
2020-12-01 12:06:32 +0100

    clean up

--- Day1/day1.hs
+++ Day1/day1.hs
@@ -1,13 +1,13 @@
 parseInputSum2 :: IO ()
 parseInputSum2 = do input <- readFile "input.txt"
-                    print . findValue2 . (map (\ x -> read x :: Int)) . words $ input
+                    print . head. findValue2 . (map (\ x -> read x :: Int)) . words $ input
                    
 parseInputSum3 :: IO ()
 parseInputSum3 = do input <- readFile "input.txt"
-                    print . findValue3 . (map (\ x -> read x :: Int)) . words $ input
+                    print . head. findValue3 . (map (\ x -> read x :: Int)) . words $ input
                    
 findValue2 :: [Int] -> [Int]
 findValue2 list = [ a * b | a <- list, b <- list, a + b == 2020]
 
 findValue3 :: [Int] -> [Int]
-findValue3 list = [ a * b * c| a <- list, b <- list, c <- list , a + b + c == 2020]
+findValue3 list = [ a * b * c | a <- list, b <- list, c <- list , a + b + c == 2020]

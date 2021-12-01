af144a053b9ee853917fe01ab87ccc15a346c08b
Ben Fidlers <ben.fidlers@gmail.com>
2021-12-01 06:40:45 +0100

    Added year 2021

---
+++ 2021_AoC/Day01/day2.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+main :: IO ()
+main = do input <- readFile "input.txt"
+          print . count . merge . (map (\ x -> read x :: Int)) . words $ input
+
+merge :: [Int] -> [Int]
+merge list
+    | length(list) < 3 = []
+    | otherwise = (sum . take 3 $ list) : (merge (tail list))
+
+count :: [Int] -> Int
+count (x:xs) = f x xs
+    where f :: Int -> [Int] -> Int
+          f _ [] = 0
+          f nb (x:xs)
+              | x > nb = 1 + f x xs
+              | otherwise = f x xs

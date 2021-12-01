4eaef0c1a12b1b702487213e1b3ff3a1aa5236fd
Ben Fidlers <ben.fidlers@gmail.com>
2021-12-01 06:40:45 +0100

    Added year 2021

---
+++ 2021_AoC/Day01/day1.hs
@@ -0,0 +1,13 @@
+import Data.List
+
+main :: IO ()
+main = do input <- readFile "input.txt"
+          print . count . (map (\ x -> read x :: Int)) . words $ input
+
+count :: [Int] -> Int
+count (x:xs) = f x xs
+    where f :: Int -> [Int] -> Int
+          f _ [] = 0
+          f nb (x:xs)
+              | x > nb = 1 + f x xs
+              | otherwise = f x xs

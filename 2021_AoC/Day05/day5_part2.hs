9ef58cc3b2c93aa704aedd80528b7dd59c93ee36
Ben Fidlers <ben.fidlers@gmail.com>
2021-12-05 18:27:20 +0100

    cleanup

--- 2021_AoC/Day05/day5_part2.hs
+++ 2021_AoC/Day05/day5_part2.hs
@@ -3,33 +3,23 @@ import Data.List.Split
 
 main :: IO ()
 main = do input <- readFile "input.txt"
-          print . parse $ input
           print . f . parse $ input
 
---f :: [[Point]] -> Int
-f = length . filter (/= 1) . map length . group . sort . list . map createLine
---f = map createLine
-    where list = foldr (++) []
+f :: [[Point]] -> Int
+f = length . filter (/= 1) . map length . group . sort . concat . map createLine
 
 createLine :: [Point] -> [Point]
 createLine ((x1,y1):(x2,y2):[])
-    | x1 - x2 == y1 - y2 && x1 > x2 = [(x1-d,y1-d) | d <- [0..x1-x2]]
-    | x1 - x2 == y1 - y2 && x2 >= x1 = [(x1+d,y1+d) | d <- [0..x2-x1]]
-    | x1 - x2 == y2 - y1 && x1 > x2 = [(x1-d,y1+d) | d <- [0..x1-x2]]
-    | x1 - x2 == y2 - y1 && x2 >= x1 = [(x1+d,y1-d) | d <- [0..x2-x1]]
-    | x1 == x2 = if y1 < y2 then [(x1,y) | y <- [y1..y2]]
-                 else [(x1,y) | y <- [y2..y1]]
-    | y1 == y2 = if x1 < x2 then [(x,y1) | x <- [x1..x2]]
-                 else [(x,y1) | x <- [x2..x1]]
+    | x1 - x2 == y1 - y2  = [(x1+d,y1+d) | d <- [0,dx..x2-x1]]
+    | x1 - x2 == y2 - y1 = [(x1+d,y1-d) | d <- [0,dx..x2-x1]]
+    | x1 == x2 = [(x1,y) | y <- [y1,y1+dy..y2]]
+    | y1 == y2 = [(x,y1) | x <- [x1,x1+dx..x2]]
     | otherwise = []
-    where minX = min x1 x2
-          maxX = max x1 x2
-          minY = min y1 y2
-          maxY = max y1 y2
+    where dx = div (x2-x1) (abs (x2-x1))
+          dy = div (y2-y1) (abs (y2-y1))
 
 type Point = (Int, Int)
 
---parse :: String -> [(Point, Point)]
 parse :: String -> [[Point]]
 parse input = coords
     where line = lines input

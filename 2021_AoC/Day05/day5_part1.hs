54844d5f1f2d3b9b8828ae72c68f8174a5deabba
Ben Fidlers <ben.fidlers@gmail.com>
2021-12-05 18:27:20 +0100

    cleanup

--- 2021_AoC/Day05/day5_part1.hs
+++ 2021_AoC/Day05/day5_part1.hs
@@ -6,16 +6,15 @@ main = do input <- readFile "input.txt"
           print . f . parse $ input
 
 f :: [[Point]] -> Int
-f = length . filter (/= 1) . map length . group . sort . list . map createLine
-    where list = foldr (++) []
+f = length . filter (/= 1) . map length . group . sort . concat . map createLine
 
 createLine :: [Point] -> [Point]
 createLine ((x1,y1):(x2,y2):[])
-    | x1 == x2 = if y1 < y2 then [(x1,y) | y <- [y1..y2]]
-                 else [(x1,y) | y <- [y2..y1]]
-    | y1 == y2 = if x1 < x2 then [(x,y1) | x <- [x1..x2]]
-                 else [(x,y1) | x <- [x2..x1]]
+    | x1 == x2 = [(x1,y) | y <- [y1,y1+dy..y2]]
+    | y1 == y2 = [(x,y1) | x <- [x1,x1+dx..x2]]
     | otherwise = []
+    where dx = div (x2 - x1) (abs (x2 - x1))
+          dy = div (y2 - y1) (abs (y2 - y1))
 
 type Point = (Int, Int)
 

08c2449e5484fa92f4cd9cb614378f583242ac6e
Ben Fidlers <ben.fidlers@gmail.com>
2021-12-12 07:41:11 +0100

    solution day 12

---
+++ 2021_AoC/Day12/day12_part2.hs
@@ -0,0 +1,46 @@
+import Data.Char
+import Data.List
+import Data.List.Split
+
+main :: IO ()
+main = do input <- readFile "input.txt"
+          print . length . paths [["start"]] . parse $ input
+
+extendable :: [[String]] -> Bool 
+extendable = not . all (\x -> head x == "end")
+
+paths :: [[String]] -> [(String,String)] -> [[String]]
+paths p rules 
+    | extendable p = paths next rules
+    | otherwise = p
+    where next = concat . map (extend rules) $ p
+
+extend :: [(String,String)] -> [String] -> [[String]]
+extend rules l@(x:xs)
+    | x == "end" = [l]
+    | otherwise = if null next then [] 
+                  else map (\a -> a:l) next
+    where next = [b | (a,b) <- rules, a == x, exception b l] ++ 
+                 [a | (a,b) <- rules, b == x, exception a l] 
+
+exception :: String -> [String] -> Bool
+exception x l
+    | bigCave x = True
+    | x == "start" = False
+    | poss l = True
+    | otherwise = not . elem x $ l
+
+poss :: [String] -> Bool
+poss input
+    | null filtered = False
+    | otherwise = all (\x -> length x < 2) . group . sort $ filtered
+    where filtered = filter (smallCave) input
+
+smallCave :: String -> Bool
+smallCave (x:_) = isLower x
+
+bigCave :: String -> Bool
+bigCave (x:_) = isUpper x
+
+parse :: String -> [(String,String)]
+parse = map ((\(x:y:[]) -> (x,y)) . splitOn "-") . lines

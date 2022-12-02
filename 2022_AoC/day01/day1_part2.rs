afa255b970692502bcdb92621b0e2380135c1aab
Ben Fidlers <ben.fidlers@gmail.com>
2022-12-02 07:06:41 +0100

    clean up

--- 2022_AoC/day01/day1_part2.rs
+++ 2022_AoC/day01/day1_part2.rs
@@ -1,17 +1,20 @@
-use std::fs;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let data = fs::read_to_string("input.txt")?;
-    let blocks: Vec<&str> = data.split("\n\n").collect();
-    let mut data2 = blocks.iter().map(|x| x.lines()
-                                  .map(|y| y.parse::<i32>().unwrap())
-                                  .sum::<i32>())
-                             .collect::<Vec<i32>>();
+    let data = std::fs::read_to_string("input.txt")?;
+    let blocks: Vec<&str> = data.split("\n\n")
+                                .collect();
+    let mut data2 = blocks.iter()
+                          .map(|x| x.lines()
+                                    .map(|y| y.parse::<i32>()
+                                              .unwrap())
+                                    .sum::<i32>())
+                          .collect::<Vec<i32>>();
 
     data2.sort_by(|a, b| b.cmp(a));
 
-    let _sum: i32 = data2[0..3].iter().sum();
+    let result: i32 = data2.iter()
+                           .take(3)
+                           .sum();
 
-    println!("{:?}", _sum);
+    println!("{}", result);
     Ok(())
 }

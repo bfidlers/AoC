4203d24b2523004e98324ae446771d9d59cd8552
Ben Fidlers <ben.fidlers@gmail.com>
2022-12-02 14:44:55 +0100

    clean up

--- 2022_AoC/day02/day2_part2.rs
+++ 2022_AoC/day02/day2_part2.rs
@@ -1,11 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("input.txt")?;
-    let data: Vec<Vec<&str>> = input.lines()
+    let data: Vec<Vec<char>> = input.lines()
                                     .map(|x| x.split_whitespace()
+                                              .map(|y| y.chars()
+                                                        .next()
+                                                        .unwrap())
                                               .collect())
                                     .collect();
 
-    let result: i32 = data.iter()
+    let result: u32 = data.iter()
                           .map(|x| get_score(x))
                           .sum();
 
@@ -13,36 +16,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn get_score(values: &Vec<&str>) -> i32 {
-    let score = match values[1] {
-        "X" => 0,
-        "Y" => 3,
-        "Z" => 6,
-        _ => 0,
-    };
-    score + get_score2(values[0], values[1])
+fn get_score(v: &Vec<char>) -> u32 {
+    let frst = v[0].to_digit(36).unwrap() - 'A'.to_digit(36).unwrap() + 1;
+    let snd  = (v[1].to_digit(36).unwrap() - 'X'.to_digit(36).unwrap()) * 3;
+    snd + get_score2(frst, snd)
 }
 
-fn get_score2(first: &str, snd: &str) -> i32 {
-    match first {
-        "A" => match snd {
-                   "X" => 3,
-                   "Y" => 1,
-                   "Z" => 2,
-                   _ => 0,
-        },
-        "B" => match snd {
-                   "X" => 1,
-                   "Y" => 2,
-                   "Z" => 3,
-                   _ => 0,
-        },
-        "C" => match snd {
-                   "X" => 2,
-                   "Y" => 3,
-                   "Z" => 1,
-                   _ => 0,
-        },
-        _ => 0,
+fn get_score2(frst: u32, snd: u32) -> u32 {
+    let value = (snd / 3 + frst + 2) % 3;
+    match value {
+        0 => 3,
+        _ => value
     }
 }

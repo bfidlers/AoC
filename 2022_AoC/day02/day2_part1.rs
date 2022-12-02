b7d9bf5725ec46bef471844caa66e182dc0f1317
Ben Fidlers <ben.fidlers@gmail.com>
2022-12-02 14:44:55 +0100

    clean up

--- 2022_AoC/day02/day2_part1.rs
+++ 2022_AoC/day02/day2_part1.rs
@@ -1,48 +1,28 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("input.txt")?;
-    let data: Vec<Vec<&str>> = input.lines()
+    let data: Vec<Vec<char>> = input.lines()
                                     .map(|x| x.split_whitespace()
+                                              .map(|y| y.chars()
+                                                        .next()
+                                                        .unwrap())
                                               .collect())
                                     .collect();
 
-    let result: i32 = data.iter()
+    let result: u32 = data.iter()
                           .map(|x| get_score(x))
                           .sum();
 
     println!("{:?}", result);
+
     Ok(())
 }
 
-fn get_score(values: &Vec<&str>) -> i32 {
-    let score = match values[1] {
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3,
-        _ => 0,
-    };
-    score + get_match_score(values[0], values[1])
+fn get_score(values: &Vec<char>) -> u32 {
+    let frst = values[0].to_digit(36).unwrap() - 'A'.to_digit(36).unwrap() + 1;
+    let snd = values[1].to_digit(36).unwrap() - 'X'.to_digit(36).unwrap() + 1;
+    snd + get_match_score(frst, snd)
 }
 
-fn get_match_score(first: &str, snd: &str) -> i32 {
-    match first {
-        "A" => match snd {
-                   "X" => 3,
-                   "Y" => 6,
-                   "Z" => 0,
-                   _ => 0,
-        },
-        "B" => match snd {
-                   "X" => 0,
-                   "Y" => 3,
-                   "Z" => 6,
-                   _ => 0,
-        },
-        "C" => match snd {
-                   "X" => 6,
-                   "Y" => 0,
-                   "Z" => 3,
-                   _ => 0,
-        },
-        _ => 0,
-    }
+fn get_match_score(frst: u32, snd: u32) -> u32 {
+    (4 + snd - frst) % 3 * 3
 }

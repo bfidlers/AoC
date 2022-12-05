17a6afc1599d0b2bf7f3a699920024331d1f2676
Ben Fidlers <ben.fidlers@gmail.com>
2022-12-05 08:44:23 +0100

    solution day 5

---
+++ 2022_AoC/day05/day05_part1.rs
@@ -0,0 +1,74 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let data: Vec<&str> = input.split("\n\n")
+                               .collect();
+    let mut crates = parse_crates(data[0]);
+    let moves = parse_moves(data[1]);
+
+    for m in moves {
+        move_crates(&m, &mut crates);
+    }
+
+    let result:String = crates.iter()
+                              .map(|x| x.last()
+                                        .unwrap())
+                              .collect::<Vec<&char>>()
+                              .into_iter()
+                              .collect();
+
+    println!("{:?}", result);
+
+    Ok(())
+}
+
+fn parse_crates(s: &str) -> Vec<Vec<char>> {
+    let test: Vec<Vec<String>> = s.lines()
+                                  .map(|x| x.chars()
+                                            .collect::<Vec<char>>()
+                                            .chunks(4)
+                                            .map(|y| y.iter()
+                                                      .collect::<String>())
+                                            .collect())
+                                  .collect();
+    let test2 = test.iter()
+                    .map(|x| x.iter()
+                              .map(|y| y.chars()
+                                        .nth(1)
+                                        .unwrap())
+                              .collect())
+                    .collect();
+    transpose(test2)
+}
+
+fn transpose(m: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut t = Vec::new();
+    for j in 0..m[0].len() {
+        let mut v = Vec::new();
+        for i in (0..m.len()-1).rev() {
+            if m[i][j].is_alphabetic() {
+                v.push(m[i][j]);
+            }
+        }
+        t.push(v);
+    }
+    t
+}
+
+fn parse_moves(s: &str) -> Vec<Vec<i32>> {
+    s.lines().map(|x| x.split_whitespace()
+                       .map(|y| y.parse::<i32>())
+                       .filter(|y| y.is_ok())
+                       .map(|y| y.unwrap())
+                       .collect())
+             .collect()
+}
+
+fn move_crates(m: &Vec<i32>, crates: &mut Vec<Vec<char>>) {
+    let x = crates[(m[1] - 1) as usize].pop()
+                                       .unwrap();
+    crates[(m[2] - 1) as usize].push(x);
+
+    if m[0] > 1 {
+        move_crates(&vec![m[0] - 1, m[1], m[2]], crates);
+    }
+}

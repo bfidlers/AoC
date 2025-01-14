5ca3ec48a9aeb45a44f88f67865621ca84bc4d10
Ben Fidlers <ben.fidlers@qmino.com>
2025-01-14 15:17:23 +0100

    add solution 2015 day 4

---
+++ 2015_AoC/day04/day04_part1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "fmt"
+  "crypto/md5"
+  "encoding/hex"
+)
+
+// const secret_key_test = "abcdef"
+const secret_key = "yzbqklnj"
+
+func startsWith(s string, c byte, n int) bool {
+  for i := 0; i < n; i++ {
+    if s[i] != c {
+      return false
+    }
+  }
+  return true
+}
+
+func findHash(s string) int {
+  for i := 0; i >= 0; i++ {
+    text := fmt.Sprintf("%s%d", s, i)
+    hash := md5.Sum([]byte(text))
+    s := hex.EncodeToString(hash[:])
+    if startsWith(s, '0', 5) {
+      return i
+    }
+  }
+  return -1
+}
+
+func main() {
+  fmt.Println(findHash(secret_key))
+}
